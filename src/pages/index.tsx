a44a6ea3ff19d7a894262a076ad9eb29c6e588ae
Gabriel Martins <martinsgabriel1956@gmail.com>
2021-09-28 12:43:34 -0300

    feat: Static Site Generation (SSG)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import Head from "next/head";
 
 import styles from '../styles/pages/Home.module.scss'
@@ -32,7 +32,7 @@ export default function Home({ product }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1Jegf6LIfhXY2oKyYUwT6ToG');
 
   const product = {
@@ -46,6 +46,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product 
-    }
+    },
+    revalidate: 60 * 60 * 24 // 24 hours
   }
 }
