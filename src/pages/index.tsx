53f44fc5c84f582077fc009db60e9b6815d36b5e
Gabriel Martins <martinsgabriel1956@gmail.com>
2021-09-27 23:49:29 -0300

    feat: Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,21 @@
 import Head from "next/head";
 
+import styles from '../styles/pages/Home.module.scss'
+
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Inicio | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
-      <h1>Hello World!</h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey Welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>Get access to all the publications <br/> <span>for 9.90 month</span></p>
+        </section>
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   );
 }
