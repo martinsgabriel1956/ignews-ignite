81706decb27c9cdd7e02c1a1ac15db84a626b22d
Gabriel Martins <martinsgabriel1956@gmail.com>
2021-10-12 11:30:34 -0300

    feat: consuming Prismic API

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,4 +1,9 @@
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
+import Prismic from '@prismicio/client';
+
+import { getPrismicClient } from '../../services/prismic';
+
 import styles from '../../styles/pages/Posts.module.scss';
 
 export default function Posts() {
@@ -30,3 +35,22 @@ export default function Posts() {
    </>
   );
 };
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query([
+    Prismic.Predicates.at('document.type', 'post'),
+
+  ], {
+    fetch: ['post.title', 'post.content'],
+    pageSize: 100,
+  })
+
+  console.log(JSON.stringify(response, null, 2));
+
+  return {
+    props: {
+      
+    }
+  }
+}
