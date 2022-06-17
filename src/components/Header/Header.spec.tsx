ec463cc445554b9480551bfba95f32354cedc5ee
Gabriel Martins <martinsgabriel1956@gmail.com>
2022-06-16 22:56:35 -0300

    feat: Testing Header component

---
+++ src/components/Header/Header.spec.tsx
@@ -0,0 +1,27 @@
+import { render, screen } from "@testing-library/react";
+import { Header } from ".";
+
+jest.mock('next/router', () => {
+  return {
+    useRouter: () => ({
+      asPath: '/',
+    })
+  }
+})
+
+jest.mock('next-auth/client', () => {
+  return {
+    useSession: () => [null, false]
+  }
+})
+
+describe('ActiveLink component', () => {
+  it('renders correctly', () => { 
+    render(
+      <Header />
+        
+    );
+    expect(screen.getByText('Home')).toBeInTheDocument();
+    expect(screen.getByText('Posts')).toBeInTheDocument();
+   })
+})
